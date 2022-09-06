4303956ea13391590a8bbc7601ca8b47e87c725f
Aileen Bryand <aileenale09@gmail.com>
2022-09-05 20:10:35 -0500

    user-controller

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -1,13 +1,107 @@
-const express = require('express');
-const mongoose = require('mongoose');
+const { User, Thought } = require("../models");
 
-const app = express();
-const PORT = process.env.PORT || 3001;
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+const userController = {
+    // Get all users
 
-app.use(express.static('public'));
+    getAllUser(req, res) {
+        User.find({})
+            .select("-__v")
+            .then((dbUserData) => res.json(dbUserData))
+            .catch((err) => {
+                console.log(err);
+                res.status(400).json(err);
+            });
+    },
 
-app.use(require('./routes'));
+    // Get one user by id
+    getUserById({ params }, res) {
+        User.findOne({ _id: params.id })
+            .populate({
+                path: "thoughts",
+                select: "-__v",
+            })
+            .populate({
+                path: "friends",
+                select: "-__v",
+            })
+            .select("-__v")
+            .then((dbUserData) => {
+                if (!dbUserData) {
+                    res.status(404).json({ message: "No user found with this id!" });
+                    return;
+                }
+                res.json(dbUserData);
+            })
+            .catch((err) => {
+                console.log(err);
+                res.status(400).json(err);
+            });
+    }
+},
+
+// Create User
+createUser({ body }, res) {
+    User.create(body)
+        .then((dbUserData) => res.json(dbUserData))
+        .catch((err) => res.status(400).json(err));
+},
+
+// Update User by id
+updateUser({ params, body }, res) {
+    User.findOneAndUpdate({ _id: params.id }, body, { new: true, runValidators: true })
+        .then((dbUserData) => {
+            if (!dbUserData) {
+                res.status(404).json({ message: "No user found with this id!" });
+                return;
+            }
+            res.json(dbUserData);
+        })
+        .catch((err) => res.status(400).json(err));
+}
+
+// Delete User
+deleteUser({ params }, res) {
+    User.findOneAndDelete({ _id: params.id })
+        .then((dbUserData) => {
+            if (!dbUserData) {
+                res.status(404).json({ message: "No user found with this id!" });
+                return;
+            }   
+            res.json(dbUserData);
+        })
+        .catch((err) => res.status(400).json(err));
+}
+
+// Add Friend
+addFriend({ params }, res) {
+    User.findOneAndUpdate(
+        { _id: params.userId },
+        { $push: { friends: params.friendId } },
+        { new: true }
+    )
+        .then((dbUserData) => {
+            if (!dbUserData) {
+                res.status(404).json({ message: "No user found with this id!" });
+                return;
+            }
+            res.json(dbUserData);
+        })
+        .catch((err) => res.json(err));
+}
+
+// Delete Friend
+deleteFriend({ params }, res) {
+    User.findOneAndUpdate(
+        { _id: params.userId },
+        { $pull: { friends: params.friendId } },
+        { new: true }
+    )
+        .then((dbUserData) => res.json(dbUserData))
+        .catch((err) => res.json(err));
+
+}
+
+module.exports = userController;
+
 
